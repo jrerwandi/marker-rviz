1aeb5dccdb37bf7e467a712dea51286b5bd9368c
Julian Reza Erwandi <73013027+jrerwandi@users.noreply.github.com>
2021-12-17 18:03:52 +0700

    Update random_marker_kiri.py

--- random_marker_kiri.py
+++ random_marker_kiri.py
@@ -7,13 +7,17 @@ import numpy as np
 from std_msgs.msg import Float64, String
 from ik_arm_solver_de.msg import jr
 from geometry_msgs.msg import PoseStamped
-
+import time
 pos = [0,0,0]
 ori = [0,0,0,0]
 status = "loading"
-aaa = 0
+aaa = 1
+solved = 0
+error = 0
+Done = False
 ####1000 marker random joint   
 def makeBox(): 
+    global solved,error,Done
     marker = Marker()
     marker.header.frame_id = "base_link"
     marker.type = Marker.CUBE
@@ -27,10 +31,12 @@ def makeBox():
         marker.color.r = 0
         marker.color.g = 1
         marker.color.b = 0
+        solved +=1
     elif (status == "IK_Error"):
         marker.color.r = 1
         marker.color.g = 0
         marker.color.b = 0
+        error +=1
     else:
         marker.color.r = 0
         marker.color.g = 0
@@ -46,11 +52,21 @@ def makeBox():
     marker.pose.orientation.y = ori[1]
     marker.pose.orientation.z = ori[2]
     marker.pose.orientation.w = ori[3]
-  
+    print("position", pos)
+    print("orientation", ori)
+    print("status", status)
+    print ("Berhasil", solved)
+    print("Error", error)
+    print("Marker",aaa)
+    if (aaa >= 1000):
+        Done = True
+       # print("Done", Done)
+   
+    print("======================================================")
     return marker  
 
 def mytopic_callback(msg):
-    global status
+    global status, aaa
     status = msg.hasil
     pos[0] = msg.position_x
     pos[1] = msg.position_y
@@ -59,28 +75,26 @@ def mytopic_callback(msg):
     ori[1] = msg.orientation_y
     ori[2] = msg.orientation_z
     ori[3] = msg.orientation_w
-
+    if (Done == False):
+        marker_objectlisher = rospy.Publisher('/marker_basic', Marker, queue_size=1)
+        markerbasics_object = makeBox()
+        marker_objectlisher.publish(markerbasics_object)
+ 
+    aaa+=1
+  
 
 def run():
-   global aaa
-   rospy.Subscriber('chatter', jr, mytopic_callback)
+    rospy.Subscriber('chatter', jr, mytopic_callback)
    #rospy.Subscriber('pose_random', PoseStamped , mytopic_callback)
-   marker_objectlisher = rospy.Publisher('/marker_basic', Marker, queue_size=100)
-   markerbasics_object = makeBox()
-   marker_objectlisher.publish(markerbasics_object)
+    rospy.spin()
    
-   aaa+=1
-  
 rospy.init_node('marker_basic_node', anonymous=True)
 rate = rospy.Rate(100) 
 if __name__ == '__main__':
     try:
-          while not rospy.is_shutdown():
-              print("position", pos)
-              print("orientation", ori)
-              print("status", status)
-              print("======================================================")
-              run()
-              rate.sleep()
+         # while not rospy.is_shutdown():
+               run()
+    
+          #    rate.sleep()
     except rospy.ROSInterruptException:
         pass
